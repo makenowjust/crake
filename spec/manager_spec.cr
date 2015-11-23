40fcf64e53bf8422cbc999129a663add966bed00
TSUYUSATO Kitsune <make.just.on@gmail.com>
2015-11-23 11:36:59 +0900

    Add CRake::Manager

---
+++ spec/manager_spec.cr
@@ -0,0 +1,158 @@
+require "spec"
+require "../src/crake/manager"
+
+class Object
+  # monkey patch (it may be a bug.)
+  def !~(other)
+    !(self =~ other)
+  end
+end
+
+def manager
+  io = MemoryIO.new
+  yield CRake::Manager.new io
+  io.to_s
+end
+
+describe CRake::Manager do
+  it "should create a new instance" do
+    manager = CRake::Manager.new
+    manager.should be_a CRake::Manager
+    manager.log.should be_a CRake::Logger
+  end
+
+  it "should log messages" do
+    manager{ |m| m.debug "debug" }.empty?.should be_true
+    manager{ |m| m.info "info" }.empty?.should be_true
+    manager{ |m| m.warn "warn" }.empty?.should be_false
+    manager{ |m| m.error "error" }.empty?.should be_false
+
+    expect_raises CRake::Error, /##fatal##/ do
+      manager{ |m| m.fatal "##fatal##" }
+    end
+  end
+
+  it "should manage tasks, files, rules and namespaces" do
+    manager = CRake::Manager.new
+
+    task = CRake::SimpleTask.new "task", "desc", ["deps"], ->(i : CRake::SimpleTask::Info){ "action" }
+    manager.add_task task
+    manager.find_task("not found").should be_nil
+    manager.find_task("task").should be task
+    task = CRake::FileTask.new "file", ["deps"], ->(i : CRake::FileTask::Info){ "action" }
+    manager.add_task task
+    manager.find_task("file").should be task
+
+    rule = CRake::Rule.new /rule/, ["deps"], ->(i : CRake::Rule::Task::Info){ "action" }
+    manager.add_rule rule
+    manager.find_rule("not found").should be_nil
+    manager.find_rule("rule").not_nil!.rule.should be rule
+
+    namespace = CRake::Namespace.new "namespace", manager
+    manager.add_namespace namespace
+    manager.find_namespace("not found").should be_nil
+    manager.find_namespace("namespace").should be namespace
+  end
+
+  it "should run tasks" do
+    s1 = manager do |m|
+      task = CRake::SimpleTask.new "task", "desc", %w(), ->(i : CRake::SimpleTask::Info) do
+        i.manager.error "##test##"
+      end
+      m.add_task task
+      m.run "task"
+    end
+    (s1 =~ /##test##/).should be_truthy
+
+    s2 = manager do |m|
+      task1 = CRake::SimpleTask.new "task1", "desc", %w(task2), ->(i : CRake::SimpleTask::Info) do
+        i.manager.error "##test1##"
+      end
+      task2 = CRake::SimpleTask.new "task2", "desc", %w(), ->(i : CRake::SimpleTask::Info) do
+        i.manager.error "##test2##"
+      end
+      m.add_task task1
+      m.add_task task2
+      m.run "task1"
+    end
+    (s2 =~ /##test2##.+##test1##/m).should be_truthy
+
+    expect_raises CRake::Error, /"task" is not defined/ do
+      manager do |m|
+        m.run "task"
+      end
+    end
+
+    expect_raises CRake::Error, /"task" has the circular dependency "task"/ do
+      manager do |m|
+        task = CRake::SimpleTask.new "task", "desc", %w(task), ->(i : CRake::SimpleTask::Info) do
+          i.manager.error "##test##"
+        end
+        m.add_task task
+        m.run "task"
+      end
+    end
+  end
+
+  it "should run files" do
+    s1 = manager do |m|
+      file1 = CRake::FileTask.new "file1", %w(), ->(i : CRake::FileTask::Info) do
+        i.manager.error "##test##"
+      end
+      m.add_task file1
+      m.run "file1"
+    end
+    (s1 =~ /##test##/).should be_truthy
+
+    s2 = manager do |m|
+      file1 = CRake::FileTask.new "file1", %w(file2), ->(i : CRake::FileTask::Info) do
+        i.manager.error "##test1##"
+      end
+      file2 = CRake::FileTask.new "file2", %w(), ->(i : CRake::FileTask::Info) do
+        i.manager.error "##test2##"
+      end
+      m.add_task file1
+      m.add_task file2
+      m.run "file1"
+    end
+    (s2 =~ /##test2##.+##test1##/m).should be_truthy
+
+    s3 = manager do |m|
+      file1 = CRake::FileTask.new "file1", ["#{__DIR__}/file/test"], ->(i : CRake::FileTask::Info) do
+        i.manager.error "##file1##"
+      end
+      file2 = CRake::FileTask.new "#{__DIR__}/file/test", %w(), ->(i : CRake::FileTask::Info) do
+        i.manager.error "##file2##"
+      end
+      m.add_task file1
+      m.add_task file2
+      m.run "file1"
+    end
+    (s3 =~ /##file1##/).should be_truthy
+    (/##file2##/ !~ s3).should be_truthy
+  end
+
+  it "should run rules" do
+    s1 = manager do |m|
+      rule1 = CRake::Rule.new /rule1/, %w(), ->(i : CRake::Rule::Task::Info) do
+        i.manager.error "##rule1##"
+      end
+      m.add_rule rule1
+      m.run "rule1"
+    end
+    (s1 =~ /##rule1##/).should be_truthy
+  end
+
+  it "should run namespaces" do
+    s1 = manager do |m|
+      ns = CRake::Namespace.new "ns", m
+      m.add_namespace ns
+      task = CRake::SimpleTask.new "task", "desc", %w(), ->(i : CRake::SimpleTask::Info) do
+        i.manager.error "##ns##"
+      end
+      ns.add_task task
+      m.run "ns:task"
+    end
+    (s1 =~ /##ns##/).should be_truthy
+  end
+end
---
+++ src/crake/manager.cr
@@ -0,0 +1,250 @@
+require "./error"
+require "./namespace"
+require "./logger"
+require "./rule"
+require "./task"
+
+# A task manager.
+class CRake::Manager
+  include Namespace::Mixin
+
+  # Internal types {{{
+
+  # :nodoc:
+  record TopLevelTask, deps do
+    def name
+      "##toplevel##"
+    end
+
+    def timestamp
+      Time::MinValue
+    end
+
+    def run(manager); end
+  end
+
+  # :nodoc:
+  record FallbackFileTask, name do
+    def deps
+      [] of String
+    end
+
+    def timestamp
+      @timestamp ||= File.lstat(name).mtime
+    end
+
+    def run(manager); end
+  end
+
+  # }}}
+  # Constructor {{{
+
+  # Create a new manager.
+  def initialize(io = STDERR)
+    @namespaces = {} of String => Namespace
+    @tasks = {} of String => Task
+    @rules = [] of Rule
+    @log = Logger.new(io)
+    @spawn = true
+  end
+
+  # }}}
+  # Properties {{{
+
+  # Returns its `Logger` instance.
+  getter log
+
+  # Returns optional arguments after `--`.
+  #
+  # ```crystal
+  # require "crake/global"
+  #
+  # p MANAGER.args
+  # # You ran `crystal make.cr -- foo bar -- hello world`:
+  # # => ["hello", "world"]
+  # ```
+  getter args
+
+  # :nodoc:
+  setter args
+
+  # Returns the flag which means whether use `spawn`
+  # for running a task.
+  def spawn?; @spawn end
+
+  # :nodoc:
+  def spawn?=(@spawn) end
+
+  # }}}
+  # Task running {{{
+
+  # Runs each tasks.
+  def run(*targets)
+    run targets
+  end
+
+  # Runs each tasks.
+  def run(targets : Enumerable(String))
+    if err = resolve_deps(TopLevelTask.new(targets)).receive
+      raise err
+    end
+  end
+
+  # Resolves the task or the rule named `target`.
+  private def resolve_task(namespace, target)
+    targets = target.split(":")
+    debug "Find target #{target.inspect} in #{namespace}"
+
+    if result = namespace.resolve_task_internal targets[0..-2], targets[-1]
+      return result
+    end
+
+    if File.exists?(target)
+      info "#{target.inspect} is not defined, so use fallback task"
+      return {self, FallbackFileTask.new target}
+    end
+
+    fatal "#{target.inspect} is not defined"
+  end
+
+  # Resolves dependepcies by topological sort with DFS.
+  private def resolve_deps(target,
+                           namespace = self,
+                           deps_set = Set(String).new,
+                           circular_check = Set(String).new) : Channel::Buffered(Exception?)
+    Channel(Exception?).new(1).tap do |chan|
+      real_proc = -> do
+        begin
+          full_name = namespace.resolve_name target
+
+          info "#{full_name.inspect} starts"
+
+          debug "#{full_name.inspect}'s dependencies are #{target.deps}"
+
+          # Resolves dependencies
+
+          ts_and_chans = [] of {(-> Time), Channel::Buffered(Exception?)}
+
+          target.deps.each do |dep|
+            if circular_check.includes? dep
+              fatal "#{full_name.inspect} has the circular dependency #{dep.inspect}"
+            end
+
+            namespace, dep_task = resolve_task namespace, dep
+
+            unless deps_set.includes? dep
+              deps_set.add dep
+              circular_check.add dep
+              ts_and_chans << {
+                ->(){ dep_task.timestamp },
+                resolve_deps dep_task, namespace, deps_set, circular_check.dup
+              }
+              circular_check.delete dep
+            end
+          end
+
+          # Waits for all tasks
+
+          times = [] of Time
+
+          ts_and_chans.each do |ts_and_chan|
+            ts, dep_chan = ts_and_chan
+            if err = dep_chan.receive
+              raise err
+            end
+            times << ts.call
+          end
+
+          # Checks timestamps
+
+          timestamp = target.timestamp
+          debug "#{full_name.inspect}'s timestamp is #{timestamp}"
+
+          flag = times.any?{ |t| t >= timestamp }
+          if times.empty?
+            flag = timestamp == Time::MaxValue || timestamp == Time::MinValue
+          end
+
+          if flag && !target.is_a?(TopLevelTask)
+            target.run self
+          end
+
+          info "#{full_name.inspect} finished"
+
+          chan.send nil
+          chan.close
+
+        rescue err
+          chan.send err
+          chan.close
+        end
+      end
+
+      if spawn?
+        spawn do
+          real_proc.call
+        end
+      else
+        real_proc.call
+      end
+    end
+  end
+
+  # }}}
+  # Show tasks {{{
+
+  # Resolves the full name of task on this manager.
+  def resolve_name(task)
+    task.name
+  end
+
+  # Shows tasks managed by self.
+  #
+  # If `all` flag is `true`, it shows all tasks
+  # eihther with or without description. If not,
+  # it shows all tasks with description.
+  def show_tasks(all = false)
+    tasks = all_tasks
+    unless tasks.empty?
+      longest = tasks.map{ |name, _| name.size }.max
+      tasks
+        .map{ |name, task| {name, task} }
+        .sort_by{ |name_task| name_task[0] as String }
+        .each do |name_task|
+          name, task = name_task
+          next unless task.responds_to? :desc
+
+          if all || !task.desc.empty?
+            print "crystal make.cr"
+            if name == "default"
+              print "    #{" " * longest}"
+            else
+              print " -- #{name.ljust longest}"
+            end
+            if task.desc.empty?
+              puts
+            else
+              puts " # #{task.desc}"
+            end
+          end
+        end
+    end
+  end
+
+  # }}}
+  # Logging {{{
+
+  {% for level in Logger::Level.constants %}
+    # Logs a message at the `{{ level }}` level.
+    delegate {{ level.downcase.id }}, @log
+  {% end %}
+
+  # Logs a message at the ERROR level, then raises an error.
+  def fatal(message)
+    error message
+    raise Error.new(message)
+  end
+
+  # }}}
+end
+# vim:fdm=marker:
